afe135a9485f079bc3d8323c5af41d8266ee8136
xpat <cheftech@pinchepoutine.digital>
2021-01-08 13:04:10 -0500

    first full code commit minus api key

--- lib/services/weather.dart
+++ lib/services/weather.dart
@@ -1,7 +1,7 @@
 import 'package:clima/services/location.dart';
 import 'package:clima/services/networking.dart';
 
-const apiKey = '93fff205af9ebb9f9bc1edbd275a5f47';
+const apiKey = '';
 const openWeatherMapURL = 'https://api.openweathermap.org/data/2.5/weather';
 
 class WeatherModel {
